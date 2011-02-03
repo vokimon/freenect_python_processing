46884ef0080d3ffe71c33a7dc8f17b2b9a2d28db
David García Garzón <david.garcia@barcelonamedia.org>
2011-02-03 21:27:21 +0100

    panelgrid.py, moved to a packed format to speed up
    22 FPS -> 210 FPS, quite a boost but i still dont understand
    why strided arrays are so expensive to set and copy.
    
    Signed-off-by: David García Garzón <david.garcia@barcelonamedia.org>

--- src/panelgrid.py
+++ src/panelgrid.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-
+import ctypes
 import pygame
-from pygame.locals import QUIT, KEYDOWN
+from pygame import QUIT, KEYDOWN, K_ESCAPE, K_f
 import numpy as np
 import sys
 import pygame.surfarray
@@ -15,11 +15,19 @@ class PannelGrid :
 	"""
 	def __init__(self, name, paneSize, colsrows) :
 		fullsize = [a*b for a,b in zip(paneSize,colsrows)]
+		pygame.init()
 		pygame.display.set_caption(name)
-		pygame.display.set_mode(fullsize,0)
-		screen = pygame.display.get_surface()
-		screenBuffer = pygame.surfarray.pixels3d(screen)
-		self.splits = [np.hsplit(v,colsrows[1]) for v in np.vsplit(screenBuffer,colsrows[0])]
+		pygame.display.set_mode(fullsize,
+#			pygame.DOUBLEBUF|
+#			pygame.HWSURFACE|
+#			pygame.FULLSCREEN|
+#			pygame.OPENGL|
+			0)
+		self.screen = pygame.display.get_surface()
+		self.colsrows = colsrows
+		screenBuffer = pygame.surfarray.pixels2d(self.screen)
+		self.splits = [np.vsplit(v,self.colsrows[0]) for v in np.hsplit(screenBuffer,self.colsrows[1])]
+
 	def pane(self, col, row) :
 		"""Returns the numpy matrix for a given pane"""
 		return self.splits[col][row]
@@ -29,7 +37,8 @@ class PannelGrid :
 	def display(self) :
 		"""Dumps the panes to the actual window"""
 		pygame.display.flip()
-
+	def packed(self, r,g,b) :
+		return ctypes.c_uint32(sum(c<<shift for c,shift in zip((r,g,b), self.screen.get_shifts())))
 
 def main() :
 	import time
@@ -39,29 +48,38 @@ def main() :
 
 	pygame.init()
 	panel = PannelGrid("PannelGrid demo", size, grid)
-	nFrames = 0
+	
 	x=0
+	firstTic=time.clock()
 	tic = time.clock()
-	stop = False
+	nFrames=0
+	stop=False
 	while not stop :
 		for event in pygame.event.get() :
-			if event.type == QUIT : sys.exit(0)
+			if event.type == QUIT : 
+				stop = True
 			if event.type == KEYDOWN  :
-				if event.key == pygame.K_ESCAPE :
+				if event.key == K_ESCAPE :
 					stop = True
-		
-		panel.pane(0,0)[:,:] = 0x0ff,0,x
-		panel.pane(1,0)[:,:] = x,0x0ff-x,0
-		panel.pane(1,1)[:,:] = 0,0,0x0ff
-		panel.pane(0,1)[:,:] = x,0,0x0ff-x
+				if( event.key == K_f ):
+				   pygame.display.toggle_fullscreen()
+		panel.pane(0,0)[:,:] = panel.packed(0xff-x,0x00+x,0x00+x)
+		panel.pane(1,0)[:,:] = panel.packed(0x00+x,0xff-x,0x00+x)
+		panel.pane(1,1)[:,:] = panel.packed(0x00+x,0x00+x,0xff-x)
+		panel.pane(0,1)[:,:] = panel.packed(x,x,x)
 		panel.display()
 		sys.stdout.write(".")
 		sys.stdout.flush()
 		x+=1
+		x&=0xff
+		toc = time.clock()
+#		print "%2.5f"%(toc-tic)
+		tic = toc
 		nFrames+=1
-	elapsed = time.clock() - tic
+	wallClock = time.clock()-firstTic
 	print
-	print "%.2f FPS, %.6f ms per frame"%(float(nFrames)/elapsed, float(elapsed)*1000/nFrames)
+	print "FPS: %.4f" % (float(nFrames)/wallClock)
+	print "%.2f milliseconds per frame"%(float(wallClock)*1000/nFrames)
 
 if __name__ == "__main__" :
 	main()
