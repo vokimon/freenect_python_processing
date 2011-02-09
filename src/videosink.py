9379ec0e0acffe9e41311044dcbb9cc1ef323223
David García Garzón <david.garcia@upf.edu>
2011-02-09 17:06:22 +0100

    videosink example adapted to the new axis

--- src/videosink.py
+++ src/videosink.py
@@ -25,10 +25,10 @@ class VideoSink(object) :
 	
 if __name__ == "__main__" :
 	import sys
-	size = 480,360
+	W,H = 480,360
 	rate = 40
-	video = VideoSink(size, "test", rate=rate, byteorder="bgra")
-	image = np.ones(size, np.uint32)
+	video = VideoSink((W,H), "test", rate=rate, byteorder="bgra")
+	image = np.ones((H,W), np.uint32)
 	for color in (
 		0x00ff0000, # red
 		0x0000ff00, # green
@@ -38,15 +38,15 @@ if __name__ == "__main__" :
 		image[:] = color
 		for i in xrange(rate) : video.run(image)
 	# Turn green from left to right
-	for col in xrange(0,size[0],2) :
-		image[col,:] = 0x0000ff00
+	for col in xrange(0,W,2) :
+		image[:,col] = 0x0000ff00
 		video.run(image)
 	# Turn blue from up to down
-	for row in xrange(0,size[1],2) :
-		image[:,row] = 0x000000ff
+	for row in xrange(0,H,2) :
+		image[row,:] = 0x000000ff
 		video.run(image)
 	for r in xrange(400) :
-		video.run(np.fromfunction(lambda g,b : np.uint32(b)+0x100*g+0x10000*r,  shape=size, dtype=np.uint32))
+		video.run(np.fromfunction(lambda g,b : np.uint32(b)+0x100*g+0x10000*r,  shape=(H,W), dtype=np.uint32))
 		print ".",
 		sys.stdin.flush()
 	video.close()
